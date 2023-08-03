6f07f4d43f6b7132015d3461588819c4bfd98391
DiscreteTom <discrete_tom@outlook.com>
2023-08-04 00:06:37 +0800

    app bar, theme toggle

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout>
+    <v-app-bar>
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title>DTTK</v-app-bar-title>
+
+      <template v-slot:append>
+        <v-btn icon="mdi-theme-light-dark" @click="toggleTheme"></v-btn>
+        <v-btn icon="mdi-dots-vertical"></v-btn>
+      </template>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <slot />
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
+
+<script setup lang="ts">
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+}
+</script>

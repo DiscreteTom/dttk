1395ee19bf82df8c09ade4536d176e5d5bb6413c
DiscreteTom <discrete_tom@outlook.com>
2023-08-06 00:39:07 +0800

    chore: add home page content

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div></div>
+  <div class="d-flex flex-column justify-center" style="height: 80vh">
+    <h1 style="width: 100%; text-align: center; font-style: italic">
+      DiscreteTom's Toolkit
+    </h1>
+  </div>
 </template>
 
 <script setup lang="ts">

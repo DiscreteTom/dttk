bbdd3e3f71b28bd2c78fd1e0ff12f455b44c31d2
DiscreteTom <discrete_tom@outlook.com>
2023-08-05 14:59:27 +0800

    chore: print settings when save/load

--- composables/useSettings.ts
+++ composables/useSettings.ts
@@ -11,14 +11,17 @@ export const useSettings = () => {
     current,
     load() {
       const str = localStorage.getItem("settings");
+      console.log(`Settings loaded: ${str}`);
       if (str !== null) {
         const settings = JSON.parse(str) as Settings;
         Object.assign(current, settings);
       }
+      console.log(`Settings applied: ${JSON.stringify(current)}`);
       return current;
     },
     save() {
       localStorage.setItem("settings", JSON.stringify(current));
+      console.log(`Settings saved: ${JSON.stringify(current)}`);
     },
   };
 };
